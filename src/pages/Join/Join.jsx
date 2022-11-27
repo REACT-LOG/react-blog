1fd4ed3d563e75faa590c4770a31e3d76f0001ee
Gyuri092 <ggr0927@gmail.com>
2022-11-28 01:55:19 +0900

    feature: join 화면 생성

---
+++ src/pages/Join/Join.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import RouterNav from '../../components/Nav';
+
+export default Join = () => {
+  return (
+    <>
+      <div>
+        <RouterNav></RouterNav>
+      </div>
+    </>
+  );
+};
