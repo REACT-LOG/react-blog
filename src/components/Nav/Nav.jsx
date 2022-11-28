cc07930367e1f423ec7711e548016af0879ab45f
ckwlghks123 <ckwlghks12+github2@gmail.com>
2022-11-28 16:09:14 +0900

    Dev: Nav.jsx Route self-closing-tag로 변경

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -78,13 +78,13 @@ function RouterNav() {
         <Routes>
           {/* Route 태그를 Routes태그로 감싸지 않고 사용할 경우 에러 발생.(리액트 라우터
         v6부터) */}
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<Home />} />
 
-          <Route path="/login" element={<Login />}></Route>
+          <Route path="/login" element={<Login />} />
 
-          <Route path="/join" element={<Join />}></Route>
+          <Route path="/join" element={<Join />} />
 
-          <Route path="/write" element={<CreatePost />}></Route>
+          <Route path="/write" element={<CreatePost />} />
         </Routes>
       </Router>
     </>
