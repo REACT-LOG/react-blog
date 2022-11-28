59533c3a44d38c9b3c5295fbe1025a507fcf95cb
ckwlghks123 <ckwlghks12+github2@gmail.com>
2022-11-28 16:15:40 +0900

    Feature: 불필요한 코드 삭제

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import RouterNav from './components/Nav/Nav.jsx';
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
       <RouterNav />
     </div>
   );
