bf80319230ddfecdaa55ddb48080c99f332e5a6f
Gyuri092 <ggr0927@gmail.com>
2022-11-29 03:39:12 +0900

    feature: useRef 를 이용한 focus 처리

--- src/pages/Join/Join.jsx
+++ src/pages/Join/Join.jsx
@@ -1,5 +1,4 @@
-import React, { useRef } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useRef, useEffect } from 'react';
 import styles from './Join.module.css';
 
 const Join = () => {
@@ -8,6 +7,9 @@ const Join = () => {
   const emailRef = useRef();
   const passwordRef = useRef();
   const confirmPasswordRef = useRef();
+  useEffect(() => {
+    nameRef.current.focus();
+  }, []);
   const submitHandler = (e) => {
     e.preventDefault();
 
