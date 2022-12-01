a6f202acfd3e5bb67e2b7beb9ffe1eb2f8813f13
ckwlghks123 <ckwlghks12+github2@gmail.com>
2022-12-01 10:55:53 +0900

    로그인시 Post 버튼 여부 수정

--- src/pages/Post/Post.jsx
+++ src/pages/Post/Post.jsx
@@ -1,31 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { Viewer } from '@toast-ui/react-editor';
 import '@toast-ui/editor/dist/toastui-editor-viewer.css';
 import styles from './Post.module.css';
 import Button from '../../components/Button/Button';
+import store from '../../utils/store';
 
 const Post = ({ posts, onRemove }) => {
   const { id } = useParams();
+  const [isLogin, setIsLogin] = useState(false);
+  const loginUser = store.getData('logedInUser');
+  const currentPost = posts.find((post) => post.id === Number(id));
+
+  console.log(currentPost.author);
+
+  useEffect(() => {
+    if (loginUser && loginUser.id === currentPost.author) {
+      setIsLogin(true);
+    }
+  }, []);
 
   return (
     <div className={styles.container}>
       <div className={styles['editor-container']}>
-        {posts.map((e) =>
-          e.id === Number(id) ? (
+        {posts.map((post) =>
+          post.id === Number(id) ? (
             <>
-              <h1 className={styles.title}>{e.title}</h1>
-              <Viewer className={styles.viewer} initialValue={e.content} />{' '}
+              <h1 className={styles.title}>{post.title}</h1>
+              <Viewer
+                className={styles.viewer}
+                initialValue={post.content}
+              />{' '}
             </>
           ) : null,
         )}
       </div>
-      <div className={styles['btn-container']}>
-        <Button to={`/write/${id}`}>수정하기</Button>
-        <Button to={'/'} onClick={onRemove(id)}>
-          삭제하기
-        </Button>
-      </div>
+      {isLogin && (
+        <div className={styles['btn-container']}>
+          <Button to={`/write/${id}`}>수정하기</Button>
+          <Button to={'/'} onClick={onRemove(id)}>
+            삭제하기
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
